a5dbd1d6dd6bb3e840cd78b1528afa7abddc394b
jojokim <jojokim@umd.edu>
2018-02-16 23:33:42 -0500

    firebase

--- facepay/src/App.js
+++ facepay/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import * as firebase from 'firebase';
+
 class App extends Component {
   render() {
     return (
--- facepay/src/index.js
+++ facepay/src/index.js
@@ -4,5 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+var config = {
+   apiKey: "AIzaSyDXC_ba_hzVZrlZg1ly3nzgL9hlGNJlaxc",
+       authDomain: "airy-charmer-164016.firebaseapp.com",
+       databaseURL: "https://airy-charmer-164016.firebaseio.com",
+       projectId: "airy-charmer-164016",
+       storageBucket: "airy-charmer-164016.appspot.com",
+       messagingSenderId: "359173267747"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().enablePersistence();
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
