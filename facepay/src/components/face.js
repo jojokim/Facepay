85f2862f545450fc513231ad99f5f033d36cf9e8
jojokim <jojokim@umd.edu>
2018-02-17 01:45:40 -0500

    a

--- facepay/src/components/face.js
+++ facepay/src/components/face.js
@@ -32,86 +32,72 @@ export default class Face extends Component {
     this.processImage(imgBlob);
   };
 
-    b64toBlob(b64Data, contentType, sliceSize) {
-      contentType = contentType || '';
-      sliceSize = sliceSize || 512;
-
-      var byteCharacters = atob(b64Data);
-      var byteArrays = [];
-
-      for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-        var slice = byteCharacters.slice(offset, offset + sliceSize);
-
-        var byteNumbers = new Array(slice.length);
-        for (var i = 0; i < slice.length; i++) {
-          byteNumbers[i] = slice.charCodeAt(i);
-        }
-
-        var byteArray = new Uint8Array(byteNumbers);
-
-        byteArrays.push(byteArray);
+  b64toBlob(b64Data, contentType, sliceSize) {
+    contentType = contentType || '';
+    sliceSize = sliceSize || 512;
+    var byteCharacters = atob(b64Data);
+    var byteArrays = [];
+    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      var slice = byteCharacters.slice(offset, offset + sliceSize);
+      var byteNumbers = new Array(slice.length);
+      for (var i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
       }
-
-      var blob = new Blob(byteArrays, {type: contentType});
-      return blob;
+      var byteArray = new Uint8Array(byteNumbers);
+      byteArrays.push(byteArray);
     }
+    var blob = new Blob(byteArrays, {type: contentType});
+    return blob;
+  }
 
+  processImage(blob) {
+    var subscriptionKey = "cb2e4566d1ac4bdea478b4a3e9ec7256";
+    var uriBase = "https://eastus.api.cognitive.microsoft.com/face/v1.0/detect";
 
-    processImage(blob) {
-            var subscriptionKey = "cb2e4566d1ac4bdea478b4a3e9ec7256";
-            var uriBase = "https://eastus.api.cognitive.microsoft.com/face/v1.0/detect";
+    // Request parameters.
+    var params = {
+      "returnFaceId": "false",
+      "returnFaceLandmarks": "false",
+      "returnFaceAttributes": "emotion",
+    };
 
-            // Request parameters.
-            var params = {
-                "returnFaceId": "false",
-                "returnFaceLandmarks": "false",
-                "returnFaceAttributes": "emotion",
-            };
+    // Perform the REST API call.
+      $.ajax({
+        url: uriBase + "?" + $.param(params),
+        contentType: false,
+        processData: false,
+        // Request headers.
+        beforeSend: function(xhrObj){
+        //xhrObj.setRequestHeader("Content-Type","application/json");
+        xhrObj.setRequestHeader("Content-Type","application/octet-stream");
+        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+        },
+        type: "POST",
+        // Request body.
+        data: blob,
+      })
+      .done((data) => {
+        // Show formatted JSON on webpage.
+        $("#responseTextArea").val(JSON.stringify(data, null, 2));
+        console.log(JSON.stringify(data, null, 2));
+        if (data[0] != null) {
+          console.log(this.emotion);
+        }
+        //this part is where you process things all the function call goes here
+        faceCompare(data);
+      })
+      .fail(function(jqXHR, textStatus, errorThrown) {
+        // Display error message.
+        var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+        errorString += (jqXHR.responseText === "") ? "" : (JSON.parse(jqXHR.responseText)) ?
+        JSON.parse(jqXHR.responseText).message : JSON.parse(jqXHR.responseText).error.message;
+        alert(errorString);
+      });
+  }
 
-            // Display the image.
-            //var sourceImageUrl = document.getElementById("inputImage").value;
-            //document.querySelector("#sourceImage").src = sourceImageUrl;
+  faceCompare = (data) => {
 
-            // Perform the REST API call.
-            $.ajax({
-                url: uriBase + "?" + $.param(params),
-                contentType: false,
-                processData: false,
-                // Request headers.
-                beforeSend: function(xhrObj){
-                  //xhrObj.setRequestHeader("Content-Type","application/json");
-                  xhrObj.setRequestHeader("Content-Type","application/octet-stream");
-                  xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-                },
-                type: "POST",
-                // Request body.
-                data: blob,
-            })
-
-            .done((data) => {
-                // Show formatted JSON on webpage.
-                $("#responseTextArea").val(JSON.stringify(data, null, 2));
-                console.log(JSON.stringify(data, null, 2));
-                if (data[0] != null) {
-                    console.log(this.emotion);
-                }
-                //this part is where you process things all the function call goes here
-                faceCompare(data);
-            })
-
-            .fail(function(jqXHR, textStatus, errorThrown) {
-                // Display error message.
-                var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
-                errorString += (jqXHR.responseText === "") ? "" : (JSON.parse(jqXHR.responseText)) ?
-                    JSON.parse(jqXHR.responseText).message : JSON.parse(jqXHR.responseText).error.message;
-                alert(errorString);
-
-            });
-        };
-
-      faceCompare = (data) => {
-
-      }
+  }
 
   render() {
     /* this.autoprogo();  */
