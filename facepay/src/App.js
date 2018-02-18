c6298095b58ebfe538125eefc1954e3f0c8294a1
jojokim <jojokim@umd.edu>
2018-02-17 20:04:30 -0500

    css

--- facepay/src/App.js
+++ facepay/src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import * as firebase from 'firebase';
-import firestore from 'firebase/firestore'
+import firestore from 'firebase/firestore';
+import styles from './App.css';
+
+import img1 from './imgs/plink4.png';
+import img2 from './imgs/blink2.png';
 
 import Face from './components/face';
 import Chart from './components/chart';
 import Group from './components/group';
 
 class App extends Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      imgurl : [""];
+    }
+  }
+
+  ender() {
     return (
       <div className="App">
-        <AppBar title="FacePay"/>
+        <img src="" alt=""/>
         <Group/>
         <Face/>
         <Chart/>
